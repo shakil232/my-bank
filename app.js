0d3a3949c92cc4173dae743d129563cb6e6eb254
unknown <dxshakil2@gmail.com>
2021-02-16 02:16:52 -0800

    done

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
-
+// loginButtonHandler...
 const getLogin = () =>{
-    const inputEmail = document.getElementById('email-input').value;
-    const inputPass = document.getElementById('pass-input').value;
-     document.getElementById('login-area').style.display = 'none';
+    const inputEmail = document.getElementById('email-input').value ;
+    const inputPass = document.getElementById('pass-input').value ;
+    document.getElementById('login-area').style.display = 'none' ;
     document.getElementById('transction-Area').style.display = 'block'
+}
+
+//  depositHandler...
+const addDeposit = ()=>{
+    const depositNumber = getInputs('deposit-input')
+
+//    const depositAmount = document.getElementById('deposit-input').value ;
+//    const depositNumber = parseFloat(depositAmount) ;
+
+//    const currentDeposit = document.getElementById('current-deposit').innerText ;
+//    const currentDepositNumber = parseFloat(currentDeposit) ;
+//    const totalDeposit =  depositNumber + currentDepositNumber ;   
+//    document.getElementById('current-deposit').innerText= totalDeposit ;  
+   document.getElementById('deposit-input').value = '' ;
+
+   getUpdateSpan(depositNumber ,'current-deposit')
+   getUpdateSpan(depositNumber ,'current-Balance')
+   
+   
+}
+// updatedd
+const getUpdateSpan = (depositNumber , id) => {
+    const currentBalance = document.getElementById(id).innerText ;
+    const currentBalanceNumber = parseFloat(currentBalance) ;
+    const totalBalance = currentBalanceNumber + depositNumber ;
+    document.getElementById(id).innerText = totalBalance ;
+}
+// WithdrawHandler....
+const getWithdraw = () =>{
+   const withDraw = getInputs('Withdraw-input');
+   document.getElementById('Withdraw-input').value  = ''
+   getUpdateSpan(withDraw  ,'current-withdraw') ;
+   getUpdateSpan(-1*withDraw ,'current-Balance') ;
+   
+   
+}
+const getInputs = (id) =>{
+    const withdrawInput = document.getElementById(id).value;
+    const withdrawNumber = parseFloat(withdrawInput);
+    return withdrawNumber 
 }
