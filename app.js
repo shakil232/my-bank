5c2691d187da445667189f3a564234df2cd90993
unknown <dxshakil2@gmail.com>
2021-02-15 21:50:27 -0800

    ok

---
+++ app.js
@@ -0,0 +1,7 @@
+
+const getLogin = () =>{
+    const inputEmail = document.getElementById('email-input').value;
+    const inputPass = document.getElementById('pass-input').value;
+     document.getElementById('login-area').style.display = 'none';
+    document.getElementById('transction-Area').style.display = 'block'
+}
